e5145eb5682eba4c523370761de940daf1a2b410
Robin Ridderholt <robin.ridderholt@laget.se>
2016-08-03 09:43:59 +0200

    Implements HelloWorldFSharp plugin

---
+++ Plugins/HelloWorldFSharp/Main.fs
@@ -0,0 +1,22 @@
+﻿namespace HelloWorldFSharp
+
+open Wox.Plugin
+open System
+open System.Collections.Generic
+
+
+type Main() =
+
+    interface IPlugin with
+        member this.Init(context) =
+            ()
+
+        member this.Query(query) =
+            let list = List<Result>()
+            let res = new Result()
+            res.Title <- "Hello World from FSharp"
+            res.SubTitle <- sprintf "Query: %s" query.Search
+            res.IcoPath <- "app.png"
+
+            list.Add(res)
+            list
